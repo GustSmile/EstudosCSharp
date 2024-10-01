bc4376f2bd1556512b362690c2677ffc525a1a36
GustSmile <gustavolemessantos.dos@gmail.com>
2024-10-01 15:51:10 -0400

    Calculadora BaseLoop

--- 02_Calculadora/Calculadora.cs
+++ 02_Calculadora/Calculadora.cs
@@ -14,17 +14,36 @@ namespace Calculadora{
             Console.WriteLine("0-Sair \n ");
 
             Console.WriteLine("Digite o número:");
-            
+           
 
             while (true)
             {
                 string entradaOperacao = Console.ReadLine();
-                if (entradaOperacao=="1"){
-                    Console.WriteLine("show");
+
+                if (entradaOperacao=="1")//Soma
+                {
+                    Console.WriteLine("Operação: Somar\n");
+                }
+                else if (entradaOperacao=="2")//Subtração
+                {
+                    Console.WriteLine("Operação: Subtrair\n");
+                }
+                else if (entradaOperacao=="3")//Multiplicação
+                {
+                    Console.WriteLine("Operação: Multiplicar\n");
+                }
+                else if (entradaOperacao=="4")//Divisão
+                {
+                    Console.WriteLine("Operação: Dividir\n");
+                }
+                else if (entradaOperacao=="0")//Sair
+                {
+                    Console.WriteLine("Operação: Sair");
+                    break;
                 }
                 else
                 {
-                    break;
+                    Console.WriteLine("Valor digitado inválido, coloque novamente: ");
                 }
             }
         }
