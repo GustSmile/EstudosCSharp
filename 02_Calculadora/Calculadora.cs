3eb1f7ad0759717dc7328b9554742238c1e8ceb2
GustSmile <gustavolemessantos.dos@gmail.com>
2024-10-01 09:12:08 -0400

    CalculadoraPrograma

--- 02_Calculadora/Calculadora.cs
+++ 02_Calculadora/Calculadora.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Calculadora");
+using System;
+namespace Calculadora{
+    class Calculadora{
+        static void Main(){
+            Programa();
+        }
+        static void Programa(){
+            Console.WriteLine("Digite o número correspondente com a operação que deseja realizar");
+            Console.WriteLine("1-Soma");
+            Console.WriteLine("2-Subtração");
+            Console.WriteLine("3-Multiplicação");
+            Console.WriteLine("4-Divisão");
+            Console.WriteLine("0-Sair \n ");
+
+            Console.WriteLine("Digite o número:");
+            string entradaOperacao = Console.ReadLine();
+        }
+    }
+}
