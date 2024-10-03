54d9f0aef2b9d97b24cd9053ab1e188a722e11a1
GustSmile <gustavolemessantos.dos@gmail.com>
2024-10-03 14:45:11 -0400

    calculadora 1.0

--- 02_Calculadora/Calculadora.cs
+++ 02_Calculadora/Calculadora.cs
@@ -41,8 +41,8 @@ namespace Calculadora{
                 
                 if (numerosValidos.Contains(entradaOperacao))
                 {
-                    int valor1 = ConferirNumero("Digite o 1º Valor: ");
-                    int valor2 = ConferirNumero("Digite o 2º Valor: ");
+                    double valor1 = ConferirNumero("Digite o 1º Valor: ");
+                    double valor2 = ConferirNumero("Digite o 2º Valor: ");
                     
                     if (entradaOperacao=="1")//Soma
                     {
@@ -57,12 +57,20 @@ namespace Calculadora{
                     else if (entradaOperacao=="3")//Multiplicação
                     {
                         Console.WriteLine("Operação: Multiplicar\n");
+                        resultado = valor1 * valor2;
                     }
                     else if (entradaOperacao=="4")//Divisão
                     {
                         Console.WriteLine("Operação: Dividir\n");
+
+                        if (valor2==0){
+                            Console.WriteLine("Não é possível dividir por zero!");
+                            resultado = 0;
+                        }else{
+                            resultado = valor1/valor2;
+                        }
                     }
-                    Console.WriteLine("Resultado: "+resultado);
+                    Console.WriteLine("Resultado: "+resultado.ToString("F2"));
                 }
                 else if (entradaOperacao=="0")//Sair
                 {
