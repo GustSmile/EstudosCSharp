e695b4989660ad739270e320485800bb04d0e0b4
GustSmile <gustavolemessantos.dos@gmail.com>
2024-10-23 11:11:17 -0400

    SomaMedia 0.1

---
+++ 03_SomaMedia/SomaMedia.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace SomaMedia{
+    class SomaMedia{
+        static void Main()
+        {
+            int valor = TamanhoVetor();
+            Console.WriteLine($"Valor retornado: {valor}");
+        }
+
+        public static int TamanhoVetor()
+        {
+            while(true)
+            {
+                int quant = ConferirNumero("Quantos números deseja informar? ");
+                return quant;
+            }
+        }
+        public static int ConferirNumero(string mensagem)
+        {
+            while (true)
+            {
+                Console.WriteLine(mensagem);
+                string valorDigitado = Console.ReadLine();
+                Console.WriteLine();
+
+                if (int.TryParse(valorDigitado, out int numero))
+                {
+                    if (numero>0 && numero<11)
+                    {
+                        return numero; //Caso Valor Válido
+                    }
+                    else
+                    {
+                        Console.WriteLine("Valor Inválido!\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Valor Inválido!\n");
+                }
+            }
+        }
+    }
+}
