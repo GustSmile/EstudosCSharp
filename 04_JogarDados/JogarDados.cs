c016325df7f1851703e026e3cd02b2515a8fd99d
GustSmile <gustavolemessantos.dos@gmail.com>
2024-10-23 17:31:08 -0400

    JogarDados 0.1

---
+++ 04_JogarDados/JogarDados.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JogarDados{
+    class JogarDados{
+        static void Main()
+        {
+            List<string> Jogador1 = NomeJogador("Digite o nome do primeiro Jogador: ");
+            imprimeJogador(Jogador1);
+            List<string> Jogador2 = NomeJogador("Digite o nome do segundo Jogador: ");
+            imprimeJogador(Jogador2);
+        }
+        public static List<string> NomeJogador(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            List<string> nome = new List<string>();
+            nome.Add(Console.ReadLine());
+            return nome;
+        }
+
+        public static void imprimeJogador(List<string>Nomes)
+        {
+            foreach(string nome in Nomes)
+            {
+                Console.WriteLine($"Nome do Jogador: {nome}\n");
+            }
+        } 
+    }
+}
