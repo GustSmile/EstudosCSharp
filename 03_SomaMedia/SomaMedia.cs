82836d4035e8aa0978710560ca593740dc711399
GustSmile <gustavolemessantos.dos@gmail.com>
2024-10-23 13:29:25 -0400

    SomaMedia 0.2

--- 03_SomaMedia/SomaMedia.cs
+++ 03_SomaMedia/SomaMedia.cs
@@ -1,21 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SomaMedia{
     class SomaMedia{
         static void Main()
         {
-            int valor = TamanhoVetor();
-            Console.WriteLine($"Valor retornado: {valor}");
+            int tamanhoVetor = TamanhoVetor("Quantos números deseja informar? ");
+            Console.WriteLine($"Valor retornado: {tamanhoVetor}\n");
+            List<double> Numeros = AddNumeros(tamanhoVetor);
+            Numeros.ForEach(numero => Console.WriteLine(numero));
         }
 
-        public static int TamanhoVetor()
-        {
-            while(true)
-            {
-                int quant = ConferirNumero("Quantos números deseja informar? ");
-                return quant;
-            }
-        }
-        public static int ConferirNumero(string mensagem)
+        public static int TamanhoVetor(string mensagem)
         {
             while (true)
             {
@@ -25,20 +21,50 @@ namespace SomaMedia{
 
                 if (int.TryParse(valorDigitado, out int numero))
                 {
-                    if (numero>0 && numero<11)
+                    if (numero>2 && numero<11)
                     {
                         return numero; //Caso Valor Válido
                     }
                     else
                     {
-                        Console.WriteLine("Valor Inválido!\n");
+                        Console.WriteLine("Declare um número entre 3 e 10!\n");
                     }
+                }
+                else
+                {
+                    Console.WriteLine("Declare um número inteiro!\n");
+                }
+            }
+        }
+
+        public static double ConferirNumero(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            string valorDigitado = Console.ReadLine();
+            Console.WriteLine();
+            while(true){
+                if (double.TryParse(valorDigitado, out double numero))
+                {
+                    return numero; //Caso Valor Válido
                 }
                 else
                 {
                     Console.WriteLine("Valor Inválido!\n");
                 }
             }
+            
+        }
+
+        public static List<double> AddNumeros(int quant)
+        {
+            
+            List<double> Numeros = new List<double>();
+
+            for (int i=0;i<quant;i++){
+                double numero = ConferirNumero($"Adicione o número {i}º: ");
+                Numeros.Add(numero);
+            }
+            return Numeros;
         }
     }
 }
